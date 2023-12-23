b788b68055ac05cf3f4e2c045128c67078d487f2
jackisace <38766065+jackisace@users.noreply.github.com>
2023-12-23 19:54:54 +0000

    update

--- main.c
+++ main.c
@@ -19,45 +19,68 @@ float train_data[4][3] = {
 };
 
 
+int single = sizeof(train_data[0][0]);
+int train_count = (sizeof(train_data) / sizeof(train_data[0]));
+int x_count = (sizeof(train_data[0]) / sizeof(train_data[0][0])) - 1;
+int train_total = (sizeof(train_data) / sizeof(train_data[0][0]));
+int y_pos = (sizeof(train_data[0]) / sizeof(train_data[0][0]));
 
-
-float cost(size_t j, float w){
+float cost(int i, int j, float w){
     float result = 0.0f;
     float x , y , d = 0.0f;
-    size_t train_count = (sizeof(train_data) / sizeof(train_data[0]));
-    size_t x_count = (sizeof(train_data[0]) / sizeof(train_data[0][0])) - 1;
-    size_t y_pos = x_count + 1;
 
+    x = train_data[i][j];
+    y = x*w;
+    d = y - train_data[i][y_pos];
+    printf("d = %f ", d);
+    result = d*d;
 
-    for(size_t i = 0; i < train_count; i++){
-        x += train_data[i][j];
-        y = x*w;
-        d = y - train_data[i][y_pos];
-        result += d*d;
-    }
-    result /= train_count;
+    //result /= train_count;
     printf("result = %f, w = %f\n", result, w);
     return result;
 }
 
-void print_weights(float ** w){
-    int single = sizeof(train_data[0][0]);
-    int train_count = (sizeof(train_data) / sizeof(train_data[0]));
-    int x_count = (sizeof(train_data[0]) / sizeof(train_data[0][0])) - 1;
-    int train_total = (sizeof(train_data) / sizeof(train_data[0][0]));
-    int y_pos = x_count + 1;
+void set_weights(float * w){
+    int t = 0;
+    for(int i = 0; i < train_count; i++){
+        for(int j = 0; j < x_count; j++){
+            if(train_data[i][j] == 0.0f || train_data[i][y_pos] == 0.0f){
+                w[t] = 0.0f;
+            } else {
+                w[t] =  train_data[i][j] / train_data[i][y_pos];
+                if(w[t] > 1.0f){
+                    w[t] = 1.0f;
+                }
+            }
+  
+            printf("%f %f %f \n", w[t], train_data[i][j], train_data[i][y_pos]);
+            t++;
+        }
+    }
+}
+
+void print_weights(float * w){
+    printf("\n===============================\n");
+
+    int t = 0;
+    for(int i = 0; i < train_count; i++){
+        for(int j = 0; j < x_count; j++){
+            printf("%f ", w[t++]);
+        }
+        printf("\n");
+    }
+    printf("===============================\n");
     
 }
 
 
 int main(){
-    int single = sizeof(train_data[0][0]);
-    int train_count = (sizeof(train_data) / sizeof(train_data[0]));
-    int x_count = (sizeof(train_data[0]) / sizeof(train_data[0][0])) - 1;
-    int train_total = (sizeof(train_data) / sizeof(train_data[0][0]));
-    int y_pos = x_count + 1;
-    float ** weights = (float**)malloc(train_count*x_count*single);
-
+    
+    float * weights = calloc(train_count * x_count, single);
+    set_weights(weights);
+    print_weights(weights);
+    return 0;
+    int t = 0;
     for(int h = 0; h < train_count; h++){
         for(int j = 0; j < x_count; j++){
             float w = 1.0f;
@@ -67,28 +90,31 @@ int main(){
             float result = w;
             float direction = eps;
             int change_count = 0;
+            int changes = 0;
             int ii = 0;
             while(result > 0.01){
                 if(change_count == 0){
                     last = (float)result;
-                    result = cost(j, w + (eps*i));
-                    if(last < result){
+                    result = cost(h, j, eps*i);
+                    if(last <= result){
                         printf("CHANGE\n");
+
                         direction *= -1.0f;
                         change_count = 5;
                     } 
                     i += direction;
                 } else {
                     change_count--;
-                    result = cost(j, w + (eps*i));
+                    result = cost(h, j, eps*i);
                     i += direction;
                 }
                 printf("i = %f, last = %f ", i, last);
                 ii++;
                 
             }
-            weights[h][j] = w + (eps*i);
+            weights[t++] = eps*i;
         }
     }
+    print_weights(weights);
     free(weights);
 }
